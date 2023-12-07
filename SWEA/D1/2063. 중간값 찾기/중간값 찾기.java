d1a3630c124a42915cdca7adaef5e0f939bf6c6e
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2023-12-07 11:02:38 +0900

    [D1] Title: 중간값 찾기, Time: 138 ms, Memory: 18,776 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -14,13 +14,18 @@ public class Solution {
 		int N = Integer.parseInt(sc.readLine());
 		st = new StringTokenizer(sc.readLine());
 		
+		// 인덱스를 사용해 값에 접근할 것이기 때문에
+		// 배열 대신 ArrayList 사용
 		ArrayList<Integer> list = new ArrayList<>();
 		for(int i=0; i<N; i++) {
 			int num = Integer.parseInt(st.nextToken());
+			// 리스트에 num값 추가
 			list.add(num);
 		}
 		
+		// 리스트를 정렬한다
 		Collections.sort(list);
+		// 리스트의 중간값 출력
 		System.out.println(list.get(N/2));
 	}
 
