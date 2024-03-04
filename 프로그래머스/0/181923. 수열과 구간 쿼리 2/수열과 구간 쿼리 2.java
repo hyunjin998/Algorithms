5754e1574ee6ae1b6bce92af8a2a416a1f1a62b0
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-03-05 07:23:32 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.17 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, 1000001);
+        
+        for(int i=0; i<queries.length; i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            for(int j=s; j<=e; j++){
+                if(arr[j] > k)
+                    answer[i] = Math.min(answer[i], arr[j]);
+            }
+            if(answer[i] == 1000001) answer[i] = -1;
+        }
+        return answer;
+    }
+}
