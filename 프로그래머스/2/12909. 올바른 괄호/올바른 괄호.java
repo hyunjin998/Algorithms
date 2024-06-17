12408728e13a22d33fe569dbcf4e948b2b25fc3f
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-06-17 16:56:57 +0900

    [level 2] Title: 올바른 괄호, Time: 8.44 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,14 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        
-        // 1. s의 길이가 홀수 -> false
-        if(s.length() % 2 != 0) return !answer;
+        boolean answer = false;
+        if(s.length() % 2 != 0) return answer;
         
         int cnt = 0;
         for(int i=0; i<s.length(); i++){
+            if(cnt<0) return answer;
+            
             char ch = s.charAt(i);
-            
-            if(ch == '(') cnt++;
-            else cnt--;
-            
-            // s = "())(())" 안 됨
-            if(cnt<0) return !answer;
-        }
-
-        // 2. '(' 와 ')' 의 수는 똑같아야함
-        if(cnt != 0) return !answer;
-        return answer;
+            cnt = (ch=='(' ? cnt+1 : cnt-1);
+        } return answer = (cnt != 0 ? false : true);
     }
 }
