46d02aab2515d9d4c0743b30ad88996506473aba
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-06-26 18:54:06 +0900

    [level 1] Title: 달리기 경주, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -3,10 +3,8 @@ import java.io.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
         HashMap <String, Integer> hash = new HashMap<>();
         
-        // hash에 값 넣기
         for(int i=0; i<players.length; i++){
             hash.put(players[i], i);
         }
@@ -23,10 +21,6 @@ class Solution {
             players[value] = tmp;
             
             hash.put(callings[i], value-1);
-        }
-        
-
-        // return answer;
-        return players;
+        } return players;
     }
 }
