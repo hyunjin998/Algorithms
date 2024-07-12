d231feff880f54ad6c7c1cde89f93add4f7a88f2
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-07-12 22:07:38 +0900

    [Bronze II] Title: 고장난 시계, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/14710. 고장난 시계/고장난 시계.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(sc.readLine());
+        String answer = "X";
+
+        int hour = Integer.parseInt(st.nextToken());
+        int min = Integer.parseInt(st.nextToken());
+
+        if(hour%30 == 0 && min == 0) answer = "O";    // 정각
+        if((hour-(min/12))%30 == 0 && min%12 == 0) answer = "O";
+        System.out.println(answer);
+    }
+}
