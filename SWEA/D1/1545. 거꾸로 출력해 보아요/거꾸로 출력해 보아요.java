70c8749cc25bb62a0be3b6a89145a2c664d28bfc
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-07-16 20:45:31 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 105 ms, Memory: 19,444 KB -BaekjoonHub

--- SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
-		int N = sc.nextInt();
-        
-		while(N>=0) sb.append(N--).append(" ");
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(sc.readLine());
+
+        while(N>=0) sb.append(N--).append(" ");
+        System.out.println(sb);
+    }
 }
