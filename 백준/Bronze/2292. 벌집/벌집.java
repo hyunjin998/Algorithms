91fabd5baacce7ffea032cfefdd823b38418153b
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-07-16 13:24:30 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,30 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(sc.readLine());
 
-		int answer = 0;
-		int[] dp = new int[18259];
-		
-		dp[0] = 1;
-		dp[1] = dp[0] + 6;
-		
-		if(N <= 7) {
-			if(N == 1) answer = 1;
-			else answer = 2;
-		}
-		else {
-			for(int i=2; N > dp[i-1]; i++) {
-				dp[i] = dp[i-1] + 6*i;
-				answer = i;
-			}
-			answer += 1;
-		}
-		System.out.println(answer);
-	}
+        int answer = 0;
+        int[] dp = new int[18259];
 
+        dp[0] = 1;
+        dp[1] = dp[0] + 6;
+
+        if(N > 7) {
+            for(int i=2; N > dp[i-1]; i++) {
+                dp[i] = dp[i-1] + 6*i;
+                answer = i;
+            } answer += 1;
+        } else answer = (N==1 ? 1 : 2);
+
+        System.out.println(answer);
+    }
 }
