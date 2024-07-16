ecea5a92a36e9f766faad738b38de0a14cc5f91b
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-07-16 14:34:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -1,12 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         sb.append(":fan::fan::fan:").append("\n");
-        sb.append(":fan::").append(sc.next()).append("::fan:").append("\n");
+        sb.append(":fan::").append(sc.readLine()).append("::fan:").append("\n");
         sb.append(":fan::fan::fan:");
         System.out.println(sb);
     }
