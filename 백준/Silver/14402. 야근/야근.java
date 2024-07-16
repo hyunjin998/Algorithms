111d717b60636d2a1a41a8e7b11bf73b598c27fe
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-07-17 03:51:50 +0900

    [Silver IV] Title: 야근, Time: 372 ms, Memory: 37468 KB -BaekjoonHub

--- 백준/Silver/14402. 야근/야근.java
+++ 백준/Silver/14402. 야근/야근.java
@@ -1,6 +1,3 @@
-// boj_14402_야근
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,6 +10,7 @@ public class Main {
         StringTokenizer st;
         HashMap<String, Integer> inCount = new HashMap<>();
         HashMap<String, Integer> outCount = new HashMap<>();
+
         int q = Integer.parseInt(sc.readLine());
         int answer = 0;
 
@@ -21,27 +19,18 @@ public class Main {
             String s = st.nextToken();
             String p = st.nextToken();
 
-            if (p.equals("+")) {
-                inCount.put(s, inCount.getOrDefault(s, 0) + 1);
-            } else { // p.equals("-")
-                if (inCount.getOrDefault(s, 0) == 0) {
-                    // 사내에 없는 상태에서 나옴 -> 야근
-                    answer++;
-                } else {
-                    inCount.put(s, inCount.get(s) - 1);
-                }
+            if(p.equals("-")) {
+                if (inCount.getOrDefault(s, 0) == 0) answer++;
+                else inCount.put(s, inCount.get(s) - 1);
                 outCount.put(s, outCount.getOrDefault(s, 0) + 1);
-            }
+            } else inCount.put(s, inCount.getOrDefault(s, 0) + 1);
         }
 
-        // 퇴근 시간까지 남아 있는 직원 처리
         for (String name : inCount.keySet()) {
             int remaining = inCount.get(name);
             if (remaining > 0) {
                 answer += remaining;
             }
-        }
-
-        System.out.println(answer);
+        } System.out.println(answer);
     }
 }
