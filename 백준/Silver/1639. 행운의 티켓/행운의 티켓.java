aa2c15486d94da28471f8305537d2f633820edcc
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-07-27 20:17:06 +0900

    [Silver IV] Title: 행운의 티켓, Time: 112 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1639. 행운의 티켓/행운의 티켓.java
+++ 백준/Silver/1639. 행운의 티켓/행운의 티켓.java
@@ -1,27 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        String str = sc.readLine();
 
         int len = str.length();
         int max = 0;
 
         for(int n=1; n <= len/2; n++) {
             for (int i=0; i <= len-2*n; i++) {
-                int leftSum = 0;
-                int rightSum = 0;
+                int left = 0;
+                int right = 0;
 
                 for (int j=i; j < i+n; j++) {
-                    leftSum += str.charAt(j) - '0';
+                    left += str.charAt(j) - '0';
                 }
 
                 for (int j = i+n; j < i+2*n; j++) {
-                    rightSum += str.charAt(j) - '0';
+                    right += str.charAt(j) - '0';
                 }
 
-                if (leftSum == rightSum) max = Math.max(max, 2 * n);
+                if (left == right) max = Math.max(max, 2 * n);
             }
         } System.out.println(max);
     }
