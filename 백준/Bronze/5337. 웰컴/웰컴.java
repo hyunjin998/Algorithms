0579f14e453d35d7a72b0b63800b97faadc50016
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-08-03 15:38:07 +0900

    [Bronze V] Title: 웰컴, Time: 92 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Print();
+    }
+
+    private static void Print() {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
