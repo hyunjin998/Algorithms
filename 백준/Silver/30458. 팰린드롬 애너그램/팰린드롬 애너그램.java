0cad2435a9a524206da077bcdcd96b1227911c64
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-08-14 03:13:50 +0900

    [Silver IV] Title: 팰린드롬 애너그램, Time: 184 ms, Memory: 18360 KB -BaekjoonHub

---
+++ 백준/Silver/30458. 팰린드롬 애너그램/팰린드롬 애너그램.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int len = Integer.parseInt(br.readLine().trim());
+        String str = br.readLine().trim();
+
+        if (str.length() != len) {
+            System.out.println("No");
+            return;
+        }
+
+        int[] cnt = new int[26];
+
+        for (int i = 0; i < len; i++) {
+            cnt[str.charAt(i) - 'a']++;
+        }
+
+        if (len % 2 == 1) cnt[str.charAt(len / 2) - 'a']--;
+
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] % 2 == 1) {
+                System.out.println("No");
+                return;
+            }
+        }
+
+        System.out.println("Yes");
+    }
+}
