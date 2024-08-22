5067363f07525507088be1e9096e7b8a64439f94
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-08-22 10:20:58 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(sc.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[1001];
+        dp[0] = 0;
+        int idx = 1;
+
+        for(int i=0; idx<1001; i++){
+            for(int j=0; j<i; j++){
+                if(idx > 1000) break;
+                dp[idx] = dp[idx-1] + i;
+                idx++;
+            }
+        }
+
+        System.out.println(dp[B]-dp[A-1]);
+
+    }
+}
