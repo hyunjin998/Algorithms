e47ecc878ffeb43d677c6db7d83a3da72f4088fe
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-08-26 19:02:38 +0900

    [level 1] Title: 공원 산책, Time: 3.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,113 +1,60 @@
 class Solution {
     
-    static Point currPos;
-    static int originX;
-    static int originY;
-    static int cnt;
-    
-    static class Point {
-        int x;
-        int y;
-        
-        public Point (int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
+    static int startX = 0;
+    static int startY = 0;
+    static int moveX = 0;
+    static int moveY = 0;
     
     public int[] solution(String[] park, String[] routes) {
-        int[] answer = new int[2];
-        FindStartPoint(park, routes, 0, 0);
-        MovingRoutesDirection(park, routes);
-
-        answer[0] = currPos.x;
-        answer[1] = currPos.y;
-        return answer;
+        
+        FindStartPoint(park);
+        
+        for(String route : routes){
+            String dir = route.split(" ")[0];
+            int move = Integer.parseInt(route.split(" ")[1]);
+            
+            moveX = startX;
+            moveY = startY;
+            
+            if(!MovingRoutesDirection(park, dir, move)){
+                moveX = startX;
+                moveY = startY;
+            } else {
+                startX = moveX;
+                startY = moveY;
+            }
+        }
+        return new int[] {startX, startY};
     }
     
     
-    // 1 출발 지점 찾기
-    public void FindStartPoint(String[] park, String[] routes, int x, int y){
+    public void FindStartPoint(String[] park) {
         for(int i=0; i<park.length; i++){
-            for(int j=0; j<park[i].length(); j++){
-                if(park[i].charAt(j) == 'S'){
-                    originX = i;
-                    originY = j;
-                    currPos = new Point(originX, originY);
-                    
-                    // check
-                    // System.out.printf("originX : %d originY : %d\n", originX, originY);
-                }
+            if(park[i].contains("S")){
+                startX = i;
+                startY = park[i].indexOf("S");
+                return;
             }
-        }
-    }
-
-
-    // 2 이동하기
-    public void MovingRoutesDirection(String[] park, String[] routes){
-        for(int i=0; i<routes.length; i++){
-            char opt = routes[i].charAt(0);
-            int num = routes[i].charAt(2) - '0';
-            
-            // check
-            // System.out.printf("===== opt : %c num : %d =====\n", opt, num);
-            
-            cnt = -1;
-            switch(opt) {
-                case 'N' :{
-                    while(checkRange(park, routes, num) && cnt < num){
-                        currPos.x -= 1;
-                    }
-                    
-                    if(cnt != num) currPos.x = originX;
-                    originX = currPos.x;
-                    break;
-                }
-                
-                case 'S' :{
-                    while(checkRange(park, routes, num) && cnt < num){
-                        currPos.x += 1;
-                    }
-                    
-                    if(cnt != num) currPos.x = originX;
-                    originX = currPos.x;
-                    break;
-                }
-                    
-                case 'W' : {
-                    while(checkRange(park, routes, num) && cnt < num){
-                        currPos.y -= 1;
-                    }
-                    
-                    if(cnt != num) currPos.y = originY;
-                    originY = currPos.y;
-                    break;
-                }
-                    
-                case 'E' : {
-                    while(checkRange(park, routes, num) && cnt < num){
-                        currPos.y += 1;
-                    }
-                    
-                    if(cnt != num) currPos.y = originY;
-                    originY = currPos.y;
-                    break;
-                }
-            }
-            
-            // check
-            // System.out.printf("> x : %d y : %d\n", currPos.x, currPos.y);
         }
     }
     
-    // 3 범위체크
-    public boolean checkRange(String[] park, String[] routes, int num) {
-        if(currPos.x>=0 && currPos.x<park.length && currPos.y>=0 && currPos.y<park[0].length() && park[currPos.x].charAt(currPos.y) != 'X'){
-            // check
-            // System.out.printf("currPos.x : %d currPos.y: %d\n", currPos.x, currPos.y);
-            cnt++;
-            return true;
-        } return false;
+    public boolean MovingRoutesDirection(String[] park, String dir, int move){
+        for(int i=0; i<move; i++){
+            switch(dir){
+                case "N": moveX--; break;
+                case "S": moveX++; break;
+                case "W": moveY--; break;
+                case "E": moveY++; break;
+            }
+            
+            if(!CheckRange(park)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean CheckRange(String[] park){
+        return (moveX >= 0 && moveX < park.length && moveY >= 0 && moveY < park[0].length() && park[moveX].charAt(moveY) != 'X');
     }
 }
