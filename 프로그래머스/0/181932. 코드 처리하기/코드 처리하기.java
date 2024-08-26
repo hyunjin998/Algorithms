225caa0ad2a9759f5b951e4138c7c9864a404c5a
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-08-27 00:49:47 +0900

    [level 0] Title: 코드 처리하기, Time: 370.26 ms, Memory: 402 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -2,31 +2,12 @@ class Solution {
     public String solution(String code) {
         String ret = "";
         boolean mode = false;
-        
         for(int idx=0; idx<code.length(); idx++){
-            if(!mode){  // 짝수
-                if(code.charAt(idx) != '1'){
-                    if(idx % 2 == 0){
-                        ret += code.charAt(idx);
-                    }
-                }
-                else mode = true;
-            }
-        
-            else {  // 홀수
-                if(code.charAt(idx) != '1'){
-                    if(idx % 2 != 0){
-                        ret += code.charAt(idx);
-                    }
-                }
-                else mode = false;
-            }
-        }
-        
-        // ret이 빈 문자열일때
-        if (ret == ""){
-            ret = "EMPTY";
-        }
-        return ret;
+            char ch = code.charAt(idx);
+            if(ch != '1') {
+                if(!mode && idx % 2 == 0) ret += ch;
+                else if(mode && idx % 2 != 0) ret += ch;
+            } else mode = !mode;
+        } return ret.equals("") ? "EMPTY" : ret;
     }
 }
