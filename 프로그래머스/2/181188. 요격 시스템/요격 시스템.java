c538eb046f24149c7ed22436db65393dec226a5b
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-08-31 02:15:18 +0900

    [level 2] Title: 요격 시스템, Time: 411.70 ms, Memory: 191 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -6,9 +6,9 @@ class Solution {
         int curr = 0;
         
         Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
-        for(int i=0; i<targets.length; i++){
-            if(curr <= targets[i][0]) {
-                curr = targets[i][1];
+        for(int[] target : targets){
+            if(curr <= target[0]) {
+                curr = target[1];
                 answer++;
             }
         } return answer;
