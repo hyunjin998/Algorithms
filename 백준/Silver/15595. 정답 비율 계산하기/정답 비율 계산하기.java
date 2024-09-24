c9522a8fda2fc95bae11c1878757eac868906128
현지니어스 <66352578+hyunjin998@users.noreply.github.com>
2024-09-25 04:51:28 +0900

    [Silver III] Title: 정답 비율 계산하기, Time: 452 ms, Memory: 47864 KB -BaekjoonHub

---
+++ 백준/Silver/15595. 정답 비율 계산하기/정답 비율 계산하기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		Set<String> s = new HashSet<>();
+		Map<String, Integer> m = new HashMap<>();
+		double ac = 0;
+		double wa = 0;
+		while(t --> 0) {
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			String name = st.nextToken();
+			int k = Integer.parseInt(st.nextToken());
+			if(name.equals("megalusion")) {
+				continue;
+			}else if(k == 4 && !s.contains(name)) {
+				ac++;
+				s.add(name);
+				if(m.get(name) != null) {
+					wa += m.get(name);
+				}
+			}else if(k != 4 && !s.contains(name)) {
+				m.put(name, m.getOrDefault(name, 0) + 1);
+			}
+		}
+		if(ac == 0) sb.append(0);
+		else sb.append((ac / (ac + (wa))) * 100);
+        System.out.println(sb);
+	}
+
+}
