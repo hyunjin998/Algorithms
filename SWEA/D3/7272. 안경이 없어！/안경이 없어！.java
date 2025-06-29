0536fcd3f5efee178f3ddd2e38b0aa68469a2199
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-06-29 19:22:56 +0900

    [D3] Title: 안경이 없어!, Time: 147 ms, Memory: 38,912 KB -BaekjoonHub

---
+++ SWEA/D3/7272. 안경이 없어！/안경이 없어！.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        // char[] no_hole = {'C', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        String no_hole = "CEFGHIJKLMNSTUVWXYZ";
+        // char[] one_hole = {'A', 'D', 'O', 'P', 'Q', 'R'};
+        String one_hole = "ADOPQR";
+        // tow_hole is only 'B'
+        
+        int T = Integer.parseInt(sc.readLine());
+        for(int test_case = 1; test_case<=T; test_case++){
+            st = new StringTokenizer(sc.readLine());
+            String preStr = st.nextToken();
+            String postStr = st.nextToken();
+            
+            String answer = "DIFF";
+            int cnt = 0;
+            
+            if(preStr.length() == postStr.length()) {
+                for(int i=0; i<preStr.length(); i++){
+                    char preChar = preStr.charAt(i);
+                    char postChar = postStr.charAt(i);
+                    
+                    if(no_hole.contains(String.valueOf(preChar)) && no_hole.contains(String.valueOf(postChar))) cnt++;
+                    else if(one_hole.contains(String.valueOf(preChar)) && one_hole.contains(String.valueOf(postChar))) cnt++;
+                    else if(preStr.charAt(i) == 'B' && postStr.charAt(i) == 'B') cnt++;
+                }
+                if(cnt == preStr.length()) answer = "SAME";
+            }
+            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
