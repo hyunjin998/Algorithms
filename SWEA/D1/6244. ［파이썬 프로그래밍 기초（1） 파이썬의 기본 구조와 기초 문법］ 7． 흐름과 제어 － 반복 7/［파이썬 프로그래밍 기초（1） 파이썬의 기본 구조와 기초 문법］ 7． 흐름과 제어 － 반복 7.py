7d5ba68dccb89e68fc20920c7d072dac40901083
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-09-09 01:33:24 +0900

    [D1] Title: [파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 7. 흐름과 제어 - 반복 7, Time: 57 ms, Memory: 50,176 KB -BaekjoonHub

--- SWEA/D1/6244. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 7． 흐름과 제어 － 반복 7/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 7． 흐름과 제어 － 반복 7.py
+++ SWEA/D1/6244. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 7． 흐름과 제어 － 반복 7/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 7． 흐름과 제어 － 반복 7.py
@@ -1,6 +1,7 @@
 list = [85, 65, 77, 83, 75, 22, 98, 88, 38, 100]
-idx = 0
-while(idx < len(list)):
-    if(list[idx] < 80): list.pop(idx)
-    else: idx+=1
+i=0
+
+while(i<len(list)):
+    if(list[i] >=80): i+=1
+    else: list.pop(i)
 print(sum(list))
