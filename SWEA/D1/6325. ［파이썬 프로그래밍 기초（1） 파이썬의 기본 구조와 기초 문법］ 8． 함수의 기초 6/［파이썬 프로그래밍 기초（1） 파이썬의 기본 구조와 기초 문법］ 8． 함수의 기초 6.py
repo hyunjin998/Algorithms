a00711b947ce4b0584d4a6493527e0d0ae8fbf68
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-09-24 00:11:32 +0900

    [D1] Title: [파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 8. 함수의 기초 6, Time: 57 ms, Memory: 52,608 KB -BaekjoonHub

---
+++ SWEA/D1/6325. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 6/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 6.py
@@ -0,0 +1,6 @@
+list_origin = [2, 4, 6, 8, 10]
+print(list_origin)
+answer = "True" if(5 in list_origin) else "False"
+print(f"5 => {answer}")
+answer = "True" if(10 in list_origin) else "False"
+print(f"10 => {answer}")
