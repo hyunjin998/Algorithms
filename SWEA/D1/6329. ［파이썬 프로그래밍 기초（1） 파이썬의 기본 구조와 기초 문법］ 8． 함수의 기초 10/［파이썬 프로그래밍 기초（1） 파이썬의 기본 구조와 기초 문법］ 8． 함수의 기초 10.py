167a29ff62d7095b1b69c84986ba35be002cecb3
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-10-05 23:08:33 +0900

    [D1] Title: [파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 8. 함수의 기초 10, Time: 58 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D1/6329. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 10/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 10.py
+++ SWEA/D1/6329. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 10/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 10.py
@@ -1,10 +1,8 @@
-def countdown(x):
-    if x <= 0:
-        print("카운트다운을 하려면 0보다 큰 입력이 필요합니다.")
-    else:
-        for i in range (x, 0, -1):
+def countdown(num):
+    if(num>0):
+        for i in range(num, 0, -1):
             print(i)
-
-num = 10
+    else : print("카운트다운을 하려면 0보다 큰 입력이 필요합니다.")
+    
 countdown(0)
 countdown(10)
