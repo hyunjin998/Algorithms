e893f576602c52a0b3997b841299e43a978c2394
김현진 <66352578+hyunjin998@users.noreply.github.com>
2026-02-15 21:09:57 +0900

    [level 0] Title: 문자열 출력하기, Time: 144.96 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -2,7 +2,12 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException{
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(sc.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        bw.write(str);
+        bw.flush();
+        bw.close();
     }
 }
