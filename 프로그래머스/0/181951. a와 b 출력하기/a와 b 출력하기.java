164e3cd359e74c53c5e5309bd1ec79a93b4c3d4d
김현진 <66352578+hyunjin998@users.noreply.github.com>
2026-02-15 21:17:58 +0900

    [level 0] Title: a와 b 출력하기, Time: 160.69 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -2,11 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(sc.readLine());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        System.out.println("a = " + a + "\nb = " + b);
+        
+        bw.write("a = " + a);
+        bw.newLine();
+        bw.write("b = " + b);
+        bw.flush();
+        bw.close();
     }
 }
